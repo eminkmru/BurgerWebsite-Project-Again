67697dabaa2153261a9dc19e5f135d9d5e7ab2e9
Emin Kumru <eminkumru135@outlook.com>
2023-04-22 16:34:45 +0300

    BurgerWebsite Project Again

---
+++ src/components/Contact.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../styles/Contact.css";
+import BannerImage from "../assets/banner.png";
+
+export const Contact = () => {
+  return (
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>Bizimle İletişime Geçin</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Lütfen Adınızı Soyadınızı Giriniz..."
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Lütfen Email Giriniz..."
+          />
+          <label>Ad Soyad</label>
+          <textarea
+            name="message"
+            placeholder="Lütfen Metninizi Giriniz..."
+          ></textarea>
+        </form>
+      </div>
+    </div>
+  );
+};
